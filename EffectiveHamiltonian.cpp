47ccb3ca0fdae5105a1da99e06392b1cf6052943
Teddy Parker <tparker@physics.ucsb.edu>
2014-02-14 14:00:50 -0800

    Predicted ground state in final fDRMG step

--- EffectiveHamiltonian.cpp
+++ EffectiveHamiltonian.cpp
@@ -11,8 +11,7 @@ EffectiveHamiltonian::EffectiveHamiltonian(const MatrixXd& matFinal,
                                            int mSFinal, int skips)
     : mSFinal(mSFinal), skips(skips)
 {
-    VectorXd seed = VectorXd::Random(mSFinal * d * mSFinal * d);
-    seed /= seed.norm();
+    VectorXd seed = TheBlock::psiGround;
     gsEnergy = lanczos(matFinal, seed, TheBlock::lancTolerance);
     psiGround = seed;
 };
