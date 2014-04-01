0c0861c9182d429ac50f970401e8d7b6097d942e
Teddy Parker <tparker@physics.ucsb.edu>
2014-04-01 16:14:07 -0700

    Added connected CF

--- FreeFunctions.h
+++ FreeFunctions.h
@@ -3,17 +3,18 @@
 
 #include "EffectiveHamiltonian.h"
 
-void oneSiteExpValues(const MatrixDd& oneSiteOp, int rangeOfObservables,
-                      int currentLSys, EffectiveHamiltonian& hSuperFinal,
-                      std::vector<TheBlock>& leftBlocks,
-                      std::vector<TheBlock>& rightBlocks,
-                      std::ofstream& fileout);
-void twoSiteExpValues(const MatrixDd& firstTwoSiteOp,
-                      const MatrixDd& secondTwoSiteOp, int rangeOfObservables,
-                      int currentLSys, EffectiveHamiltonian& hSuperFinal,
-                      std::vector<TheBlock>& leftBlocks,
-                      std::vector<TheBlock>& rightBlocks,
-                      std::ofstream& fileout);
-void modifyHamParams(int trial = 0);
+Eigen::VectorXd oneSiteExpValues(const MatrixDd& oneSiteOp,
+                                 int rangeOfObservables, int currentLSys,
+                                 EffectiveHamiltonian& hSuperFinal,
+                                 std::vector<TheBlock>& leftBlocks,
+                                 std::vector<TheBlock>& rightBlocks,
+                                 std::ofstream& fileout);
+Eigen::MatrixXd twoSiteExpValues(const MatrixDd& firstTwoSiteOp,
+                                 const MatrixDd& secondTwoSiteOp,
+                                 int rangeOfObservables, int currentLSys,
+                                 EffectiveHamiltonian& hSuperFinal,
+                                 std::vector<TheBlock>& leftBlocks,
+                                 std::vector<TheBlock>& rightBlocks,
+                                 std::ofstream& fileout);
 
 #endif
