bf7f639368dcdadf4a5888a1a4b56fcef2f5c320
Teddy Parker <tparker@physics.ucsb.edu>
2014-05-09 17:44:14 -0700

    Explained Input format in readme

--- main.h
+++ main.h
@@ -3,9 +3,9 @@
 
 #include <iostream>
 #include <vector>
-#include "Eigen/Dense"
-#include "Eigen/StdVector"
-#include "unsupported/Eigen/KroneckerProduct"
+#include <Eigen/Dense>
+#include <Eigen/StdVector>
+#include <unsupported/Eigen/KroneckerProduct>
 #include "GlobalHamiltonianParameters.h"
 
 typedef Eigen::Matrix<double, d, d> MatrixDd;
